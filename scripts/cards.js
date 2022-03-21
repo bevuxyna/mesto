a7df717f5753d7ae205de0c93ac90eb64174dc58
bevuxyna <wannhorn@gmail.com>
2022-03-21 16:06:59 +0300

    feature: cards.js

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+//Массив карточек мест
+const initialCards = [
+    {
+        name: 'Байкал',
+        link: 'images/philipp-trubchenko-08lvM7w-G30-unsplash.jpg'
+    },
+    {
+        name: 'Эльбрус',
+        link: 'images/dmitrii-vaccinium-9qsK2QHidmg-unsplash.jpg'
+    },
+    {
+        name: 'Телецкое, Республика Алтай',
+        link: 'images/nick-night-v_0g5mGwHM0-unsplash.jpg'
+    },
+    {
+        name: 'Архыз',
+        link: 'images/denis-zelenykh-CvSvsqdFLcI-unsplash.jpg'
+    },
+    {
+        name: 'Рускеала',
+        link: 'images/victor-malyushev-qGcnIyX0SZU-unsplash%20(1).jpg'
+    },
+    {
+        name: 'Остров Русский',
+        link: 'images/fedor-shlyapnikov-mc3sRmbEA2o-unsplash%20(1).jpg'
+    }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -32,36 +32,6 @@ const cardTemplate = document.querySelector('.elements__item').content;
 const placeElement = document.querySelector('.elements__box');
 
 
-//Массив карточек мест
-const initialCards = [
-    {
-        name: 'Байкал',
-        link: 'images/philipp-trubchenko-08lvM7w-G30-unsplash.jpg'
-    },
-    {
-        name: 'Эльбрус',
-        link: 'images/dmitrii-vaccinium-9qsK2QHidmg-unsplash.jpg'
-    },
-    {
-        name: 'Телецкое, Республика Алтай',
-        link: 'images/nick-night-v_0g5mGwHM0-unsplash.jpg'
-    },
-    {
-        name: 'Архыз',
-        link: 'images/denis-zelenykh-CvSvsqdFLcI-unsplash.jpg'
-    },
-    {
-        name: 'Рускеала',
-        link: 'images/victor-malyushev-qGcnIyX0SZU-unsplash%20(1).jpg'
-    },
-    {
-        name: 'Остров Русский',
-        link: 'images/fedor-shlyapnikov-mc3sRmbEA2o-unsplash%20(1).jpg'
-    }
-];
-
-
-
 //Открытие popup
 function openPopup (popup) {
     popup.classList.add('popup_opened');
