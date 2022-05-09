3e2b96e82cdddf2137b57e19b23399f8b67cca01
bevuxyna <wannhorn@gmail.com>
2022-05-09 14:45:49 +0300

    feature: PopupWithConfirm.js

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -3,20 +3,21 @@ import {Popup} from "./Popup";
 export class PopupWithConfirm extends Popup{
     constructor(popupSelector) {
         super(popupSelector);
-
+        this._handleSubmit = this._submitEvent.bind(this);
     }
 
-    open() {
+    _submitEvent(evt) {
+        evt.preventDefault();
+        this._handleSubmit(this._data);
+    }
+
+    open(data) {
+        this._data = data;
         super.open();
-
-    }
-
-    close() {
-        super.close();
-
     }
 
     setEventListeners() {
+        this._popupSelector('submit', this._handleSubmit);
         super.setEventListeners();
     }
 }
