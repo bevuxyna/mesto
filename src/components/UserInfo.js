fadddc64fe8df7ce32b5ac5d866b72d1e30137fc
bevuxyna <wannhorn@gmail.com>
2022-05-09 15:04:26 +0300

    feature: set user avatar

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,24 @@
 export class UserInfo {
-    constructor({profileName, profileDescription}) {
+    constructor({profileName, profileDescription, profileAvatar}) {
         this._name = document.querySelector(profileName);
         this._info = document.querySelector(profileDescription);
+        this._avatar = document.querySelector(profileAvatar);
     }
 
 //Возвращает объект с данными пользователя
     getUserInfo() {
         this._userData = {
             userName: this._name.textContent,
-            userInfo: this._info.textContent
+            userInfo: this._info.textContent,
+            userAvatar: this._avatar.src
         }
         return this._userData;
     }
 
 //Принимает новые данные пользователя и добавляет их на страницу
-    setUserInfo({name, info}) {
+    setUserInfo({name, info, avatar}) {
         this._name.textContent = name;
         this._info.textContent = info;
+        this._avatar.src = avatar;
     }
 }
